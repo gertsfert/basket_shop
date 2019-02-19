55f4b872058078d3c4f972a245a68162db2cf5de
gertsfert <gertsfert@gmail.com>
2019-02-19 18:50:02 +1100

    register recipe app in admin panel

--- app/project/recipes/admin.py
+++ app/project/recipes/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import (IngredientType, Ingredient, Recipe,
+                     RecipeStep, RecipeIngredient)
+
+admin.site.register([IngredientType, Ingredient, Recipe,
+                     RecipeStep, RecipeIngredient])
+
 # Register your models here.
